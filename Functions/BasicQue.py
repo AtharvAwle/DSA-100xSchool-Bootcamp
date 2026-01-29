acbd7753de2aef6ca9a4080b42c42f09fc2ec8bd
AtharvAwle <atharvawle0156@gmail.com>
2026-01-29 21:54:19 +0530

    Week-3 Fn class

---
+++ Functions/BasicQue.py
@@ -0,0 +1,19 @@
+# Factorial
+def fact(n):
+    ans = 1
+    for i in range(n,1,-1):
+        ans *= i
+        
+fact(5)
+
+
+
+# sum
+def sum(a,b):
+    return a+b
+
+
+# print 1 to n
+def print1ton(n):
+    for i in rage(1,n+1):
+        print(i)
